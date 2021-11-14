8882a187be2eba9dbb3538b714b4efb0096fa49f
Kiruthiga <kiruthiga.career@gmail.com>
2021-11-14 16:27:30 +0530

    |Dipika and Kiruthiga | Added Area and Perimeter of Rectangle

--- src/main/java/com/shapes/rectangle/Rectangle.java
+++ src/main/java/com/shapes/rectangle/Rectangle.java
@@ -1,4 +1,25 @@
 package com.shapes.rectangle;
 
 public class Rectangle {
+
+    double length;
+    double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public double calculateArea()
+    {
+
+        return length * width;
+
+    }
+
+    public double calculatePerimeter()
+    {
+        return 2 *(length+width);
+
+    }
 }
--- src/test/java/com/shape/rectangle/RectangleTest.java
+++ src/test/java/com/shape/rectangle/RectangleTest.java
@@ -1,6 +1,25 @@
 package com.shape.rectangle;
 
+import com.shapes.rectangle.Rectangle;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+
 public class RectangleTest {
+    @Test
+    void calculateAreaOfRectangle() {
+        Rectangle rectangle = new Rectangle(12, 2);
+
+        assertEquals(24, rectangle.calculateArea());
+    }
+
+    @Test
+    void calculatePerimeterOfRectangle() {
+        Rectangle rectangle = new Rectangle(10.5, 2);
+
+        assertEquals(25, rectangle.calculatePerimeter());
 
 
+    }
 }
